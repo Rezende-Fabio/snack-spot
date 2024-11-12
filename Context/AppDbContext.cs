a7a8026cce15ce23044ce15811fd6cace7df2943
Rezende-Fabio <rezendefabio337@gmail.com>
2024-11-12 20:42:06 -0300

    REMOVE: remove files

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace snack_spot.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Lanche> Lanches { get; set; }
+}
--- obj/Debug/net8.0/snack-spot.AssemblyInfo.cs
+++ obj/Debug/net8.0/snack-spot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("snack-spot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f834739248114fc1813f93e6d8764b804ba698b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde538409972ef235671e142f48ab4da9e749dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("snack-spot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("snack-spot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
