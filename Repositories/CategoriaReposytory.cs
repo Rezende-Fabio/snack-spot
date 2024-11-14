3d10a6448c2048e55a542412045aea6b05b14fc2
Rezende-Fabio <rezendefabio337@gmail.com>
2024-11-13 22:24:22 -0300

    FEAT: implementing the repository pattern for Categoria

---
+++ Repositories/CategoriaReposytory.cs
@@ -0,0 +1,16 @@
+using snack_spot.Context;
+using snack_spot.Interfaces;
+using snack_spot.Models;
+
+namespace snack_spot.Repositories;
+
+public class CategoriaReposytory : ICategoriaRepository
+{
+    private readonly AppDbContext _context;
+    public CategoriaReposytory(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Categoria> Categorias => _context.Categorias;
+}
