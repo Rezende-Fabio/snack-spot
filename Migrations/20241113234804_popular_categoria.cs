838dadef02e97577822c5bee3b89e677715f2844
Rezende-Fabio <rezendefabio337@gmail.com>
2024-11-13 21:04:52 -0300

    FEAT: created migration for insert data in Categoria

---
+++ Migrations/20241113234804_popular_categoria.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace snack_spot.Migrations
+{
+    /// <inheritdoc />
+    public partial class popular_categoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO \"Categoria\" (\"Nome\", \"Descricao\") VALUES"+
+                "('Natural', 'Lanche feito com ingredientes naturais e pão integral'),"+
+                "('Caseiro', 'Lanche feito com ingredientes feitos por nós'),"+
+                "('Gourmet', 'Lanche feito com ingredientes diferenciados com um tamanho maior');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"Categoria\"");
+        }
+    }
+}
