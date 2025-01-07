18ead8ce8ad64594333118a6464f2aab03ca7b4d
Rezende-Fabio <rezendefabio337@gmail.com>
2025-01-07 20:44:36 -0300

    FEAT: create view model CarrinhoCompraViewModel

---
+++ ViewModels/CarrinhoCompraViewModel.cs
@@ -0,0 +1,9 @@
+using snack_spot.Models;
+
+namespace snack_spot.ViewModels;
+
+public class CarrinhoCompraViewModel
+{
+   public CarrinhoCompra CarrinhoCompra { get; set; }
+   public decimal TotalCarrinho { get; set; }
+}
