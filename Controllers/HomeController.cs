1033af6d1eba37baa85c069e4600859401a79c7b
Rezende-Fabio <rezendefabio337@gmail.com>
2025-01-11 19:16:58 -0300

    REFACTOR: adding list of favorite snacks to home index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using snack_spot.Interfaces;
 using snack_spot.Models;
+using snack_spot.ViewModels;
 
 namespace snack_spot.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILancheRepository _lancheRepository;
+
+    public HomeController(ILancheRepository lancheRepository)
+    {
+        _lancheRepository = lancheRepository;
+    }
 
     public IActionResult Index()
     {
-        return View();
+        var homeViewModel = new HomeViewModel{
+            LanchesPreferidos = _lancheRepository.LanchesPreferidos
+        };
+
+        return View(homeViewModel);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
