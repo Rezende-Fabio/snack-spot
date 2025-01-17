83faadfd31945fd0fcf978d352006ce8fe18a510
Rezende-Fabio <rezendefabio337@gmail.com>
2025-01-16 21:11:38 -0300

    REFACTOR: adding the route to filter the snacks

--- Program.cs
+++ Program.cs
@@ -37,7 +37,14 @@ app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "categoriaFiltro",
+    pattern: "Lanche/{action}/{categoria?}",
+    defaults: new { Controller = "Lanche", action = "List" }
+);
+
+app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
