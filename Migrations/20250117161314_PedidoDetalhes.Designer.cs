3abc3fd200476eb62dd93d7e0e898dfa7309af37
Rezende-Fabio <rezendefabio337@gmail.com>
2025-01-17 13:18:15 -0300

    FEAT: created migration for the Pedido and PedidoDetalhe table

--- Migrations/20250117161314_PedidoDetalhes.Designer.cs
+++ Migrations/20250117161314_PedidoDetalhes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using snack_spot.Context;
@@ -11,9 +12,11 @@ using snack_spot.Context;
 namespace snack_spot.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250117161314_PedidoDetalhes")]
+    partial class PedidoDetalhes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
