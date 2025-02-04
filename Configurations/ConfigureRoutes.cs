76150d00ef2c9a3bf1cf1df266ec1b8b91c714b8
Rezende-Fabio <rezendefabio337@gmail.com>
2025-02-03 21:01:48 -0300

    FEAT: creating CRUD for snack in the administrative area

--- Configurations/ConfigureRoutes.cs
+++ Configurations/ConfigureRoutes.cs
@@ -6,6 +6,11 @@ public static class ConfigureRoutes
     public static void UseAppRoutes(this WebApplication app)
     {
         app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+        );
+
+        app.MapControllerRoute(
             name: "categoriaFiltro",
             pattern: "Lanche/{action}/{categoria?}",
             defaults: new { Controller = "Lanche", action = "List" }
@@ -14,11 +19,6 @@ public static class ConfigureRoutes
         app.MapControllerRoute(
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}"
-        );
-
-        app.MapControllerRoute(
-            name: "areas",
-            pattern: "{area=exists}/{controller=Admin}/{action=Index}/{id?}"
         );
     }
 }
